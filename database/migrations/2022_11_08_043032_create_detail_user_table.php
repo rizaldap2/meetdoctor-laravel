926643781484aa4b707a92feb0f4453ca95247a6
rizaldap2 <rizaldapramana@gmail.com>
2022-11-08 12:20:16 +0700

    add migration

--- database/migrations/2022_11_08_043032_create_detail_user_table.php
+++ database/migrations/2022_11_08_043032_create_detail_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTypeUserTable extends Migration
+class CreateDetailUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,14 @@ class CreateTypeUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('type_user', function (Blueprint $table) {
+        Schema::create('detail_user', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->integer('user_id');
+            $table->integer('type_user_id');
+            $table->string('contact')->nullable();
+            $table->longText('address')->nullable();
+            $table->longText('photo')->nullable();
+            $table->enum('gender',[1,2])->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
@@ -28,6 +33,6 @@ class CreateTypeUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('type_user');
+        Schema::dropIfExists('detail_user');
     }
 }
--- database/migrations/2022_11_08_045937_create_doctor_table.php
+++ database/migrations/2022_11_08_045937_create_doctor_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTypeUserTable extends Migration
+class CreateDoctorTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,12 @@ class CreateTypeUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('type_user', function (Blueprint $table) {
+        Schema::create('doctor', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->integer('specialist_id');
+            $table->string('name');
+            $table->string('fee');
+            $table->longText('photo')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
@@ -28,6 +31,6 @@ class CreateTypeUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('type_user');
+        Schema::dropIfExists('doctor');
     }
 }
