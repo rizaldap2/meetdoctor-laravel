316ab4b43671d11c8dc805cd3fa2dae934c1a784
rizaldap2 <rizaldapramana@gmail.com>
2023-01-17 10:43:33 +0700

    fix

---
+++ resources/views/includes/backsite/meta.blade.php
@@ -0,0 +1,13 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui " />
+
+{{-- Seo Setting base --}}
+<meta name="description" content="{{ 'Meet your Doctor. Trusted & Professional' }}">
+<meta key="keywords" content="{{ 'Doctor, meetdoctor, patient, appointment, trusteed & profesional' }}">
+<meta name="author" content="{{ 'Meetdoctor' }}">
+
+{{-- CSRF token --}}
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+{{-- Favicon --}}
--- resources/views/includes/frontsite/meta.blade.php
+++ resources/views/includes/frontsite/meta.blade.php
@@ -1,3 +1,11 @@
 <meta charset="UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+{{-- Seo Setting base --}}
+<meta name="description" content="{{ 'Meet your Doctor. Trusted & Professional' }}">
+<meta key="keywords" content="{{ 'Doctor, meetdoctor, patient, appointment, trusteed & profesional' }}">
+<meta name="author" content="{{ 'Meetdoctor' }}">
+
+{{-- CSRF token --}}
+<meta name="csrf-token" content="{{ csrf_token() }}">
